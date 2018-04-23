f76fcaee50361880f388ef37eb45292a441ff5a8
Kevin Janvier <kevin.janvier5@gmail.com>
2018-04-23 20:37:32 +0300

    first commit

---
+++ app/src/main/java/com/thinkdevs/wallpaper/activities/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.thinkdevs.wallpaper.activities
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		
+		startActivity(Intent(this, HomeActivity::class.java))
+		finish()
+	
+	}
+	
+	
+	
+}
